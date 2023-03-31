3b9a19bcf618cab301cd551e6b986074b0b833ca
lvndry <lvndry@proton.me>
2023-03-31 11:23:34 +0200

    not push field if amount zero

--- apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
+++ apps/ledger-live-mobile/src/components/RootNavigator/BaseNavigator.tsx
@@ -79,6 +79,7 @@ import { BaseNavigatorStackParamList } from "./types/BaseNavigator";
 import DeviceConnect, {
   deviceConnectHeaderOptions,
 } from "../../screens/DeviceConnect";
+import NoFundsFlowNavigator from "./NoFundsFlowNavigator";
 import ExploreTabNavigator from "./ExploreTabNavigator";
 import StakeFlowNavigator from "./StakeFlowNavigator";
 import { RecoverPlayer } from "../../screens/Protect/Player";
@@ -89,7 +90,6 @@ import {
   NavigationHeaderCloseButtonAdvanced,
 } from "../NavigationHeaderCloseButton";
 import EditTransactionNavigator from "../../families/ethereum/EditTransactionNavigator";
-import NoFundsFlowNavigator from "./NoFundsFlowNavigator";
 
 const Stack = createStackNavigator<BaseNavigatorStackParamList>();
 
