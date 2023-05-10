b2943f57dd07cb96600fc6af9bfedf6b3e0fcbc8
drako <dany.rakotopara@ledger.fr>
2023-05-10 21:40:44 +0200

    w

--- apps/ledger-live-desktop/src/renderer/modals/Send/steps/StepPro.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/Send/steps/StepPro.tsx
@@ -6,9 +6,18 @@ import { Alert, Text, Divider, Flex } from "@ledgerhq/react-ui";
 import { withV3StyleProvider } from "~/renderer/styles/StyleProviderV3";
 import Label from "~/renderer/components/Label";
 import Item from "./Pro/Item";
-import axios from 'axios';
+import axios from "axios";
 
-const StepPro = ({ status, selectedProIndex, setSelectedProIndex, pending, setPending }: StepProps) => {
+const myPubKey = "2cdfa3ff8cf9b7cfe4d61d21dea82c20f83f43761f44b4c4daede52f9873e2a68";
+const org = "test_hk_4";
+
+const StepPro = ({
+  status,
+  selectedProIndex,
+  setSelectedProIndex,
+  pending,
+  setPending,
+}: StepProps) => {
   const wrappedOnSetSelectedProIndex = useCallback(
     newIndex => {
       // Toggle if reclicked;
@@ -18,23 +27,24 @@ const StepPro = ({ status, selectedProIndex, setSelectedProIndex, pending, setPe
   );
 
   useEffect(() => {
-    axios.get('https://ledger-live-pro.minivault.ledger-sbx.com/router/test_hk7/dashboard')
-        .then(response => {
-          console.log(response.data);
-          const transactions = response.data["pending_transactions"];
-          const pendingTransactions = transactions.map(transaction => {
-            return {
-              memo: transaction.memo,
-              hash: transaction.raw_tx,
-              validators: [transaction.approvals.length, 3]
-            };
-          });
-          console.log(pendingTransactions);
-          setPending(pendingTransactions);
-        })
-        .catch(error => {
-          console.error(error);
+    axios
+      .get(`https://ledger-live-pro.minivault.ledger-sbx.com/router/${org}/dashboard`)
+      .then(response => {
+        console.log(response.data);
+        const transactions = response.data.pending_transactions;
+        const pendingTransactions = transactions.map(transaction => {
+          return {
+            memo: transaction.memo,
+            hash: transaction.raw_tx,
+            validators: [transaction.approvals.length, 3],
+          };
         });
+        console.log(pendingTransactions);
+        setPending(pendingTransactions);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, []);
 
   if (!status) return null;
@@ -72,54 +82,65 @@ const StepPro = ({ status, selectedProIndex, setSelectedProIndex, pending, setPe
 
 export const StepProFooter = ({ selectedProIndex, transitionTo, pending }: StepProps) => {
   const onApprove = async () => {
-      // transitionTo("device"); to do when we have the app
-      // api call after the device thinigy
+    // transitionTo("device"); to do when we have the app
+    // api call after the device thinigy
 
-      const data = {
-          "pub_key": "CCCCC",
-          "raw_tx": pending[selectedProIndex]["hash"],
-          "signature": "0xsig3"
-      };
-      axios.post("https://ledger-live-pro.minivault.ledger-sbx.com/router/test_hk7/transaction/approve", data)
-          .then(response => {
-              console.log(response.data);
-          })
-          .catch(error => {
-              console.error(error);
-          });
+    const data = {
+      pub_key: myPubKey,
+      raw_tx: pending[selectedProIndex].hash,
+      signature: "0xsig3",
+    };
+    axios
+      .post(
+        `https://ledger-live-pro.minivault.ledger-sbx.com/router/${org}/transaction/approve`,
+        data,
+      )
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   const onNewTransaction = async () => {
     // transitionTo("recipient"); to do when we have the app
-      // api call after the device thinigy
-      const data = {
-          "memo": "test_txe",
-          "pub_key": "CCCCC",
-          "raw_tx": pending[selectedProIndex]["hash"],
-          "signature": "0xsig3"
-      };
-      axios.post("https://ledger-live-pro.minivault.ledger-sbx.com/router/test_hk7/transaction/initiate", data)
+    // api call after the device thinigy
+    const data = {
+      memo: "test aljdasjdtxe",
+      pub_key: myPubKey,
+      raw_tx: "0xoeesdfrere",
+      signature: "0xsig3",
+    };
+    axios
+      .post(
+        `https://ledger-live-pro.minivault.ledger-sbx.com/router/${org}/transaction/initiate`,
+        data,
+      )
+      .then(response => {
+        console.log(response.data);
+        // then approve tx
+        const approveData = {
+          pub_key: myPubKey,
+          raw_tx: "0xoeereresd",
+          signature: "0xsig3w",
+        };
+        axios
+          .post(
+            `https://ledger-live-pro.minivault.ledger-sbx.com/router/${org}/transaction/initiate`,
+            approveData,
+          )
           .then(response => {
-              console.log(response.data);
-              // then approve tx
-              const approveData = {
-                  "pub_key": "CCCCC",
-                  "raw_tx": pending[selectedProIndex]["hash"],
-                  "signature": "0xsig3w"
-              }
-              axios.post("https://ledger-live-pro.minivault.ledger-sbx.com/router/test_hk7/transaction/initiate", approveData)
-                  .then(response => {
-                      console.log(response.data);
-                      // then approve tx
-                  })
-                  .catch(error => {
-                      console.error(error);
-                  });
-
+            console.log(response.data);
+            // then approve tx
           })
           .catch(error => {
-              console.error(error);
+            console.error(error);
           });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   return (
